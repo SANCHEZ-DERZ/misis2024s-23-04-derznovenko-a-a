2f39b8fac59a5e640ae264d08bfc5ce86c01e468
Alexander <derznovenko2005@mail.ru>
2024-02-13 00:06:03 +0300

    Changed StackArr

--- prj.lab/stackarr/stackarr.cpp
+++ prj.lab/stackarr/stackarr.cpp
@@ -33,5 +33,57 @@ StackArr::~StackArr() {
 
 StackArr& StackArr::operator=(const StackArr& arr) {
 	size_ = arr.size_;
-	i_top_
+	i_top_ = arr.i_top_;
+	delete[] data_;
+	data_ = new Complex[size_]{};
+	for (int i = 0; i < size_; i++) {
+		data_[i] = arr.data_[i];
+	}
+	return *this;
 }
+
+
+bool StackArr::IsEmpty() const noexcept {
+	if (size_ == 0) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+
+Complex& StackArr::Top() {
+	if (IsEmpty) {
+		throw std::out_of_range("Stack is empty");
+	}
+	else {
+		i_top_ = size_ - 1;
+		return data_[i_top_];
+	}
+}
+
+
+void StackArr::Push(const Complex& val) {
+	if (size_ != 0) {
+		size_ += 1;
+		Complex* new_data = new Complex[size_]{};
+		for (int i = 0; i < size_; i++) {
+			new_data[i] = data_[i];
+		}
+		delete[] data_;
+		data_ = new_data;
+		data_[size_ - 1] = val;
+	}
+	else {
+		size_ += 1;
+		data_ = new Complex[size_];
+		data_[0] = val;
+	}
+}
+
+
+void StackArr::Pop() noexcept {
+}
+
+
