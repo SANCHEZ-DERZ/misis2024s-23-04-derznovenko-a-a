4cfc3e3840cba547b9abc0103905bb8292327821
Alexander <derznovenko2005@mail.ru>
2024-03-21 19:56:46 +0300

    fixed stackarr

--- prj.test/test_queuelst.cpp
+++ prj.test/test_queuelst.cpp
@@ -1,78 +1,92 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
-#include <queuelst/queuelst.hpp>
+#include "queuelst/queuelst.hpp"
 
-TEST_CASE("cons") {
-    SUBCASE("default") {
-        QueueLst a;
-        CHECK(a.Size() == 0);
-    }
-    SUBCASE("copy") {
-        QueueLst a;
-        for (int i = 0; i < 500; ++i)
-            a.Push(Complex(1, i));
 
-        QueueLst b(a);
-        CHECK(b.Size() == 500);
-        CHECK(b.Top() == a.Top());
 
-        b.Push(Complex());
-        CHECK(b.Size() == 501);
-        CHECK(a.Size() == 500);
-        CHECK(b.Top() == a.Top());
-    }
+TEST_CASE("stack IsEmpty") {
+	QueueLst st;
+	Complex a{ 1.5, 2.0 };
+	Complex b{ 2.0, 3.0 };
+	CHECK_EQ(st.IsEmpty(), 1);
+	st.Push(a);
+	CHECK_EQ(st.Top(), a);
 }
 
-TEST_CASE("appr") {
-    SUBCASE("appr with empty") {
-        QueueLst a;
-        QueueLst b;
-        for (int i = 0; i < 5; ++i)
-            b.Push(Complex(1, i));
-        a = b;
-        CHECK(a.Size() == 5);
-        CHECK(b.Top() == a.Top());
-
-        b.Push(Complex());
-        CHECK(b.Size() == 6);
-        CHECK(a.Size() == 5);
-        CHECK(b.Top() == a.Top());
-    }
-    SUBCASE("appr with full") {
-        QueueLst a;
-        QueueLst b;
-        for (int i = 0; i < 5; ++i)
-        {
-            a.Push(Complex(1, i));
-            b.Push(Complex(i, 2));
-        }
-        a = b;
-        CHECK(a.Size() == 5);
-        CHECK(b.Top() == a.Top());
-        b.Push(Complex());
-        CHECK(b.Size() == 6);
-        CHECK(a.Size() == 5);
-        CHECK(b.Top() == a.Top());
-    }
+TEST_CASE("Push") {
+	QueueLst st;
+	Complex a{ 1.5, 2.0 };
+	Complex b{ 2.0, 3.0 };
+	CHECK_EQ(st.IsEmpty(), 1);
+	st.Push(a);
+	st.Push(b);
+	CHECK_EQ(st.Top(), a);
 }
 
-TEST_CASE("Push, Pop & Top") {
-    QueueLst a;
-    CHECK(a.Size() == 0);
-    for (int i = 0; i < 1000; ++i)
-    {
-        a.Push(Complex(i, i));
-        CHECK(a.Size() == i + 1);
-        CHECK(a.Top() == Complex(0, 0));
-    }
+TEST_CASE("Pop") {
+	QueueLst st;
+	Complex a{ 1.5, 2.0 };
+	Complex b{ 1.0, 1.0 };
+	st.Push(a);
+	st.Push(b);
+	CHECK_EQ(st.IsEmpty(), 0);
+	st.Pop();
+	CHECK_EQ(st.Top(), b);
+	st.Pop();
+	CHECK_EQ(st.IsEmpty(), 1);
+}
 
-    for (int i = 1; i < 1000; ++i)
-    {
-        a.Pop();
-        CHECK(a.Size() == 1000 - i);
-        CHECK(a.Top() == Complex(i, i));
-    }
-    a.Pop();
-    CHECK_THROWS(a.Top());
-    a.Pop();
+TEST_CASE("Top") {
+	QueueLst st;
+	Complex a{ 1.5, 2.0 };
+	st.Push(a);
+	CHECK_EQ(st.Top(), a);
+}
+
+TEST_CASE("Clear") {
+	QueueLst st;
+	Complex a{ 1.5, 2.0 };
+	st.Push(a);
+	st.Clear();
+	CHECK_EQ(st.IsEmpty(), 1);
+}
+
+TEST_CASE("Cycle") {
+	QueueLst st;
+	Complex a;
+	float b = 0;
+	for (int i = 0; i < 1000; i++) {
+		b += 1.0;
+		a = { b, 0 };
+		st.Push(a);
+	}
+	CHECK_EQ(st.Top(), Complex{ 1, 0 });
+	st.Pop();
+	CHECK_EQ(st.Top(), Complex{ 2, 0 });
+	CHECK_EQ(st.IsEmpty(), 0);
+	st.Clear();
+	CHECK_EQ(st.IsEmpty(), 1);
+}
+
+
+TEST_CASE("Copy") {
+	QueueLst st;
+	Complex a{ 1.5, 2.0 };
+	Complex b{ 2.0, 3.0 };
+	st.Push(a);
+	st.Push(b);
+	QueueLst cpy(st);
+	CHECK_EQ(cpy.Top(), st.Top());
+}
+
+
+TEST_CASE("Operator=") {
+	QueueLst st;
+	QueueLst cpy;
+	Complex a{ 1.5, 2.0 };
+	Complex b{ 2.0, 3.0 };
+	st.Push(a);
+	st.Push(b);
+	cpy = st;
+	CHECK_EQ(cpy.Top(), st.Top());
 }
