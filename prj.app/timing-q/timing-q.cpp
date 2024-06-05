36690df8cf96cb8cb90dce6c12e84bcef65a429a
Alexander <derznovenko2005@mail.ru>
2024-06-05 17:37:01 +0300

    fixed

--- prj.app/timing-q/timing-q.cpp
+++ prj.app/timing-q/timing-q.cpp
@@ -77,4 +77,17 @@ int main() {
 }
 
 
-
+//#include <iostream>
+//#include <matplot/matplot.h>
+//#include <set>
+//#include <thread>
+//#include <vector>
+//
+//int main() {
+//    using namespace matplot;
+//
+//    gca();
+//
+//    show();
+//    return 0;
+//}
